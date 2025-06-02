3a141f9aa2816d96d43ec5440e302387fe685afb
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-02 15:09:24 +0900

    switch->if

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,16 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        String mood = "tired";
+        String mood = "angry";
 
-        switch (mood) {
-            case "happy":
-                System.out.println("나는 행복합니다.");
-
-            case "angry":
-                System.out.println("나는 화났습니다.");
-                break;
-
-            case "sad":
-                System.out.println("나는 슬픕니다");
-                break;
-
-            default:
-                System.out.println("지금 내 기분을 표현할 수 없습니다.");
-                break;
+        if(mood == "happy") {
+            System.out.println("나는 행복합니다.");
+        } else if(mood == "angry") {
+            System.out.println("나는 화났습니다.");
+        } else if(mood == "sad") {
+            System.out.println("나는 슬픕니다.");
+        } else {
+            System.out.println("지금 내 기분을 표현할 수 없습니다.");
         }
     }
 }
